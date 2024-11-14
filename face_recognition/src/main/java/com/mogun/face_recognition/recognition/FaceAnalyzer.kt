bcd61649bdc1db1cd795c9f1233c7aa9626d64c5
seungohjin <corrobolea@gmail.com>
2024-11-14 12:04:05 +0900

    update: 얼굴 인식 모듈 기능 구현

--- face_recognition/src/main/java/com/mogun/face_recognition/recognition/FaceAnalyzer.kt
+++ face_recognition/src/main/java/com/mogun/face_recognition/recognition/FaceAnalyzer.kt
@@ -1,6 +1,9 @@
 package com.mogun.face_recognition.recognition
 
+import android.graphics.PointF
+import android.graphics.RectF
 import android.media.Image
+import android.util.SizeF
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import androidx.camera.view.PreviewView
@@ -11,6 +14,7 @@ import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.face.Face
 import com.google.mlkit.vision.face.FaceDetection
 import com.google.mlkit.vision.face.FaceDetectorOptions
+import kotlin.math.abs
 
 internal class FaceAnalyzer(
     lifecyCle: Lifecycle,
@@ -19,6 +23,12 @@ internal class FaceAnalyzer(
 ) : ImageAnalysis.Analyzer {
     private var widthScaleFactor = 1F
     private var heightScaleFactor = 1F
+
+    private var preCenterX = 0F
+    private var preCenterY = 0F
+    private var preWidth = 0F
+    private var preHeight = 0F
+
 
     private val options = FaceDetectorOptions.Builder()
         .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)  // 정확도
@@ -40,22 +50,26 @@ internal class FaceAnalyzer(
             } else if (detectStatus == FaceAnalyzerStatus.Detect &&
                 (face.leftEyeOpenProbability ?: 0F) > EYE_SUCCESSS_VALUE &&
                 (face.rightEyeOpenProbability ?: 0F) < EYE_SUCCESSS_VALUE
-                ) {
+            ) {
                 detectStatus = FaceAnalyzerStatus.LeftWink
                 listener?.detectProgress(50F, "오른쪽 눈만 깜빡여주세요.")
-            } else if(detectStatus == FaceAnalyzerStatus.LeftWink &&
+            } else if (detectStatus == FaceAnalyzerStatus.LeftWink &&
                 (face.leftEyeOpenProbability ?: 0F) < EYE_SUCCESSS_VALUE &&
-                (face.rightEyeOpenProbability ?: 0F) > EYE_SUCCESSS_VALUE) {
+                (face.rightEyeOpenProbability ?: 0F) > EYE_SUCCESSS_VALUE
+            ) {
                 detectStatus = FaceAnalyzerStatus.RightWink
                 listener?.detectProgress(75F, "활짝 웃어보세요.")
-            } else if(detectStatus == FaceAnalyzerStatus.RightWink &&
-                (face.smilingProbability ?: 0F) > SMILE_SUCCESSS_VALUE) {
+            } else if (detectStatus == FaceAnalyzerStatus.RightWink &&
+                (face.smilingProbability ?: 0F) > SMILE_SUCCESSS_VALUE
+            ) {
                 detectStatus = FaceAnalyzerStatus.Smile
                 listener?.detectProgress(100F, "얼굴 인식이 완료되었습니다.")
                 listener?.stopDetect()
                 dectector.close()
             }
-        } else if(detectStatus != FaceAnalyzerStatus.UnDetect && detectStatus != FaceAnalyzerStatus.Smile) {
+
+            calDetectSize(face)
+        } else if (detectStatus != FaceAnalyzerStatus.UnDetect && detectStatus != FaceAnalyzerStatus.Smile) {
             detectStatus = FaceAnalyzerStatus.UnDetect
             listener?.notDetect()
             listener?.detectProgress(0F, "얼굴을 인식하지 못했습니다. \n 처음으로 돌아갑니다.")
@@ -89,9 +103,47 @@ internal class FaceAnalyzer(
             }
     }
 
+    private fun calDetectSize(face: Face) {
+        val rect = face.boundingBox
+        val boxWidth = rect.right - rect.left
+        val boxHeight = rect.bottom - rect.top
+
+        val left = (rect.right.translateX() - (boxHeight / 2)).toFloat()
+        val top = (rect.top.translateY() - (boxWidth / 2)).toFloat()
+        val right = (rect.left.translateX() + (boxHeight / 2)).toFloat()
+        val bottom = (rect.bottom.translateY() + (boxWidth / 2)).toFloat()
+
+        val width = right - left
+        val height = bottom - top
+        val centerX = left + width / 2
+        val centerY = top + height / 2
+
+        if (abs(preCenterX - centerX) > PIVOT_OFFSET
+            || abs(preCenterY - centerY) > PIVOT_OFFSET
+            || abs(preWidth - width) > SIZE_OFFSET
+            || abs(preHeight - height) > SIZE_OFFSET
+        ) {
+            listener?.faceSize(
+                RectF(left, top, right, bottom),
+                SizeF(width, height),
+                PointF(centerX, centerY)
+            )
+
+            preCenterX = centerX
+            preCenterY = centerY
+            preWidth = width
+            preHeight = height
+        }
+    }
+
+    private fun Int.translateX() = preview.width - (toFloat() * widthScaleFactor).toInt()
+    private fun Int.translateY() = (toFloat() * heightScaleFactor).toInt()
+
     companion object {
         private const val EYE_SUCCESSS_VALUE = 0.1F
         private const val SMILE_SUCCESSS_VALUE = 0.8F
 
+        private const val PIVOT_OFFSET = 15
+        private const val SIZE_OFFSET = 30
     }
 }
